db123f9d13d97a879d0348d9a6e67bb62877995d
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2023-01-07 22:12:53 +0700

    feat: Add HomeScreen in main

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/screen/home_screen.dart';
+
+void main() => runApp(const MovieApp());
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MovieApp',
+      theme: ThemeData(),
+      home: const HomeScreen(),
+    );
+  }
+}
