e21a9085dc378709ed73dbc7303742cc46bf625f
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2023-01-09 08:52:08 +0700

    feat: Update MovieDetailScreen layout

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -40,7 +40,7 @@ class HomeScreen extends StatelessWidget {
             return InkWell(
               onTap: (() {
                 Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return const MovieDetailScreen();
+                  return MovieDetailScreen(movie: movie);
                 }));
               }),
               child: Card(
--- lib/screen/movie_detail_screen.dart
+++ lib/screen/movie_detail_screen.dart
@@ -1,11 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/model/movie.dart';
 
 class MovieDetailScreen extends StatelessWidget {
-  const MovieDetailScreen({super.key});
+  final Movie movie;
+  const MovieDetailScreen({required this.movie, super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.vertical(bottom: Radius.circular(16)),
+                  child: Image.network(
+                    imageUrl + movie.backdropPath,
+                    height: 220,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 120),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.network(
+                      imageUrl + movie.posterPath,
+                      width: 120,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 240, left: 152, right: 16),
+                  child: Text(
+                    movie.title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        overflow: TextOverflow.ellipsis),
+                    maxLines: 2,
+                  ),
+                ),
+                SafeArea(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back,
+                        )),
+                    const WatchListButton()
+                  ],
+                )),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                movie.overview,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WatchListButton extends StatefulWidget {
+  const WatchListButton({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _WatchListButtonState();
+}
+
+class _WatchListButtonState extends State<WatchListButton> {
+  bool isWatchList = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          setState(() {
+            isWatchList = !isWatchList;
+          });
+        },
+        icon: Icon(isWatchList ? Icons.bookmark : Icons.bookmark_outline));
   }
 }
